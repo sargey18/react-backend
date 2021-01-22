0fcc8eb6d28d67a90120be771e6ec5482aa95213
Ryan Sargeson <35943901+sargey18@users.noreply.github.com>
2021-01-22 15:07:28 +0000

    update

--- server.js
+++ server.js
@@ -113,6 +113,6 @@ app.put('/image', (req, res) =>{
 
 
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log(`app is running on port ${process.env.PORT}`)
 })
